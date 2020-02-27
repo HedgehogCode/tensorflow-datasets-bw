bed5a0d26c8f42314ed41b32082367690a2d56f2
Benjamin Wilhelm <benjamin.wilhelm@uni-konstanz.de>
2020-02-27 14:46:33 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='TensorFlow Datasets BW',
+    version='0.0.1',
+    description='Extensions to TensorFlow Datasets',
+    author='Benjamin Wilhelm',
+    author_email='benjamin.wilhelm@uni-konstanz.de',
+    url='https://github.com/HedgehogCode/tensorflow-datasets-bw',
+    packages=['tensorflow_datasets_bw'],
+    install_requires=[
+        'scipy',
+        'tensorflow>=2',
+        'tensorflow-datasets>=2',
+    ],
+)
