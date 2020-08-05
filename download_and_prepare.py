6d4dbd66920263c401bb069bab93ef9ac246cef4
Benjamin Wilhelm <benjamin.wilhelm@uni-konstanz.de>
2020-08-05 09:33:51 +0200

    Fix linting errors

--- download_and_prepare.py
+++ download_and_prepare.py
@@ -1,10 +1,8 @@
 """A wrapper for the script tensorflow_datasets.scripts.download_and_prepare which
 imports tensorflow_datasets_bw."""
-import subprocess
-
 from absl import app
 import tensorflow_datasets.scripts.download_and_prepare as script
-import tensorflow_datasets_bw
+import tensorflow_datasets_bw  # noqa: F401
 
 if __name__ == '__main__':
     app.run(script.main)
--- tensorflow_datasets_bw/mdsp_color_sr.py
+++ tensorflow_datasets_bw/mdsp_color_sr.py
@@ -5,8 +5,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-import tensorflow as tf
 import tensorflow_datasets.public_api as tfds
 import scipy.io
 import numpy as np
